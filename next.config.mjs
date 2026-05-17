d1665d6bea72085a7a2c63342f88943e76eb3d93
Ameen SR <ameensr25@gmail.com>
2026-05-17 14:06:51 +0530

    Configure trailingSlash, correct basePath, and set up automatic gh-pages branch deployment via GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/aadilshajabbar-porfolio' : '',
+  basePath: isProd ? '/aadilshajabbar-portfolio' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
